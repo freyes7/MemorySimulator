640d43b5589266586c8b41034ee6f128a56b97d2
FiacroReyes <fgrl1408@gmail.com>
2019-05-03 16:34:38 -0500

    initialization, validation and tests added

--- client.py
+++ client.py
@@ -12,17 +12,70 @@ sock.connect(server_address)
 try:
     
     # Send data
-    message = 'wed'
+    message = " "
     sock.sendall(message)
 
     data = sock.recv(4096)
-    print >>sys.stderr, '"%s"' % data
+    print >>sys.stderr, '%s' % data
+
+    message = "RealMemory noSoyUnEntero"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "RealMemory 34"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "SwapMemory 34"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "PageSize 34"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "PoliticaMemory SSTF"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "PoliticaMemory LIFO"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "F"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "PoliticaMemory FIFO"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
+
+    message = "F"
+    sock.sendall(message)
+
+    data = sock.recv(4096)
+    print >>sys.stderr, '%s' % data
 
     message = 'E'
     sock.sendall(message)
 
     data = sock.recv(4096)
-    print >>sys.stderr, '"%s"' % data
+    print >>sys.stderr, '%s' % data
 finally:
-    print >>sys.stderr, 'closing socket'
     sock.close()
--- server.py
+++ server.py
@@ -3,6 +3,94 @@ import sys
 
 #Controlers
 finished = False
+state = 0
+
+#Global variables
+realMemorySize = 0
+swapMemorySize = 0
+pageSize = 0
+politica = 'FIFO'
+
+#Functions
+def setRealMemorySize(data):
+    command = data.split()
+    if len(command)<2 : 
+        return '\nInsuficientes parametros'
+    if command[0] != 'RealMemory': 
+        return '\nEl comando no es el esperado, intenta de nuevo.'
+    try:
+        realMemorySize = int(command[1])
+    except ValueError:
+        return '\nIngresa un entero para el tamanio de la memoria'
+    global state
+    state = state + 1
+    return '\nEl tamanio de la memoria se cambio con exito'
+
+def setSwapMemorySize(data):
+    command = data.split()
+    if len(command)<2 : 
+        return '\nInsuficientes parametros'
+    if command[0] != 'SwapMemory': 
+        return '\nEl comando no es el esperado, intenta de nuevo.'
+    try:
+        swapMemorySize = int(command[1])
+    except ValueError:
+        return '\nIngresa un entero para el tamanio de la memoria'
+    global state
+    state = state + 1
+    return '\nEl tamanio de la memoria se cambio con exito'
+
+def setPageSize(data):
+    command = data.split()
+    if len(command)<2 : 
+        return '\nInsuficientes parametros'
+    if command[0] != 'PageSize': 
+        return '\nEl comando no es el esperado, intenta de nuevo.'
+    try:
+        pageSize = int(command[1])
+    except ValueError:
+        return '\nIngresa un entero para el tamanio de las paginas'
+    global state
+    state = state + 1
+    return '\nEl tamanio de las paginas se cambio con exito'
+
+def setPolitica(data):
+    command = data.split()
+    if len(command)>=1 and command[0] == 'E': 
+        finished = True
+        return '\nHasta Luego'
+    if len(command)<2 : 
+        return '\nInsuficientes parametros'
+    if command[0] != 'PoliticaMemory': 
+        return '\nEl comando no es el esperado, intenta de nuevo.'
+    if command[1] != 'FIFO' and command[1] != 'LIFO':
+        return '\nDicha Politica no es manejada por el programa'
+    global state
+    state = state + 1
+    politica = command[1]
+    return '\nLa politica de remplazo se cambio con exito'
+
+def instruction(data):
+    command = data.split()
+    if len(command)>=1 and command[0]=='F':
+        global state
+        state = state - 1
+        return '\nElige la nueva politica de remplazo o ingresa E para terminar'
+    return '\nError'
+
+def process(data):
+    global state
+    if state == 0:
+        return setRealMemorySize(data)
+    elif state == 1:
+        return setSwapMemorySize(data)
+    elif state == 2:
+        return setPageSize(data)
+    elif state == 3:
+        return setPolitica(data)
+    elif state == 4:
+        return instruction(data)
+    return '\nError'
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,11 +111,10 @@ while not finished:
         while True:
             data = connection.recv(4096)
             if data: 
-                connection.sendall('Recibido ' + data)
+                connection.sendall('Recibido ' + data + process(data))
                 if data=='E': finished = True
             else:
                 break
     finally:
         # Clean up the connection
         connection.close()
-print("Hasta luego")
