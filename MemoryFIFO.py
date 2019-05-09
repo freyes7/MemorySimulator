4cf859525ef23f1c108ef91e668e39e9eea91b2b
FiacroReyes <fgrl1408@gmail.com>
2019-05-09 11:14:20 -0500

    LIFO and FIFO specialization deleted

--- MemoryFIFO.py
+++
@@ -1,5 +0,0 @@
-from Memory import Memory
-class MemoryFIFO(Memory):
-
-        def loadProcess(self, process, processSize):
-                return ''
--- MemoryLIFO.py
+++
@@ -1,5 +0,0 @@
-from Memory import Memory
-class MemoryLIFO(Memory):
-
-        def loadProcess(self, process, processSize):
-                return ''
